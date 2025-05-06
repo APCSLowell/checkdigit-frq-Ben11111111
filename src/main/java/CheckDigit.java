495470002bce04eea020438f15170dff6ab6228d
Ben11111111 <ben042554@gmail.com>
2025-05-06 08:06:04 -0700

    Update CheckDigit.java

--- src/main/java/CheckDigit.java
+++ src/main/java/CheckDigit.java
@@ -1,45 +1,30 @@
 public class CheckDigit {   
-    /** Returns the check digit for num, as described in part (a).  
-     *  Precondition: The number of digits in num is between one and    
-     *  six, inclusive.  
-     *  num >= 0  
-     */  
+    private int total, n;
+
     public static int getCheck(int num) {  
         int sum = 0;
-        int numDigits = getNumberOfDigits(num);
-        for (int i = 0; i < numDigits; i++) {
-            sum += getDigit(num, i + 1) * (8 - i);
+        for(int i = 0; i <= getNumberOfDigits(num); i++){
+            sum += (8 - 1) * getDigit(num, i);
         }
         return sum % 10;
     }
 
-    /** Returns true if numWithCheckDigit is valid, or false    
-     *  otherwise, as described in part (b). 
-     *  Precondition: The number of digits in numWithCheckDigit   
-     *  is between two and seven, inclusive.
-     *  numWithCheckDigit >= 0     
-     */     
     public static boolean isValid(int numWithCheckDigit) {      
-        return numWithCheckDigit % 10 == getCheck(numWithCheckDigit / 10);
-    }
+        return numWithCheckDigit % 10 == getCheck(numWithCheckDigit / 10); 
+    }    
 
-    /** Returns the number of digits in num. */    
     public static int getNumberOfDigits(int num) {      
-        if (num < 10) {
+        if(num < 10)
             return 1;
-        }
         return 1 + getNumberOfDigits(num / 10);    
-    }
+    }    
 
-    /** Returns the nth digit of num.      
-     *  Precondition: n >= 1 and n <= the number of digits in num     
-     */    
     public static int getDigit(int num, int n) {      
-        int pos = getNumberOfDigits(num) - n;
-        while (pos > 0) {
+        int pos = getNumberOfDigits(num) - n + 1;
+        while(pos > 1){
             num /= 10;
             pos--;
         }
         return num % 10;
-    }
+    }     
 }
